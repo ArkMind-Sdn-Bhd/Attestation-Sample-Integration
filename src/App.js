28f297d5dca6302d6a48cbc00134701223c728a9
Draden Liang Han Sheng <hanshengliang@outlook.com>
2022-09-04 11:48:51 +0800

    :tada: Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <iframe src={"http://35.197.149.75:3000"} width={"80%"} height={"600vh"} allow="camera;fullscreen;accelerometer;gyroscope;magnetometer" allowFullScreen/>
     </div>
   );
 }
