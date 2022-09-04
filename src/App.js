fe594a8a870a24cd013539a973de463f7dea67dc
Draden Liang Han Sheng <hanshengliang@outlook.com>
2022-09-04 12:07:14 +0800

    :zap: Updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <iframe src={"http://35.197.149.75:3000"} width={"80%"} height={"600vh"} allow="camera;fullscreen;accelerometer;gyroscope;magnetometer" allowFullScreen/>
+      <iframe src={"http://35.197.149.75:3000"} width={"80%"} height={"800vh"} allow="camera;fullscreen;accelerometer;gyroscope;magnetometer" allowFullScreen/>
     </div>
   );
 }
